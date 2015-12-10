a458c2f7bdeff3bbafc01e9e5e17bab3b1ee92fb
Kayvan Sylvan <kayvansylvan@gmail.com>
2015-12-10 11:53:31 -0800

    Add to run.sh ability to start, stop, restart container.

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 #
-docker run -d -p 1080:1080 -p 1143:1143 -p 1389:1389 \
-  -p 1110:1110 -p 1025:1025 -v ~/.davmail:/etc/davmail kayvan/davmail
+
+NAME=davmail
+PORTS="-p 1080:1080 -p 1143:1143 -p 1389:1389 -p 1110:1110 -p 1025:1025"
+
+case "$1" in
+  start)
+    docker run --name=$NAME -d $PORTS -v ~/.davmail:/etc/davmail kayvan/davmail
+    ;;
+  stop)
+    docker rm -f $NAME
+    ;;
+  restart)
+    $0 stop; $0 start;
+    ;;
+  *)
+    echo "Usage: $0 [start|stop|restart]"
+    ;;
+esac
